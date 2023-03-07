55c557a0281c9036cae70e153bedb4738cb71b52
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-03-07 18:15:21 -0500

    HW Class 04

---
+++ src/class04/class04HWTask2.java
@@ -0,0 +1,25 @@
+package class04;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class class04HWTask2 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        //open site
+        driver.get("http://practice.syntaxtechs.net/basic-checkbox-demo.php");
+        driver.manage().window().maximize();
+        List<WebElement> checkBoxes=driver.findElements(By.xpath("//input[@class='cb1-element']"));
+        for(WebElement w:checkBoxes){
+           String box=w.getAttribute("value");
+           if(box.equals("Option-2")){
+               w.click();
+           }
+        }
+    }
+}
