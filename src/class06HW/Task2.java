1f16e32241cea299b16def9dc333e1387ba3e983
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-03-09 14:14:05 -0500

    Class 06 HW

---
+++ src/class06HW/Task2.java
@@ -0,0 +1,27 @@
+package class06HW;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","Drivers/chromedriver");
+        WebDriver driver=new ChromeDriver();
+        //open site
+        driver.get("https://the-internet.herokuapp.com/iframe");
+        driver.manage().window().maximize();
+        WebElement iframe=driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']"));
+        //switching to iframe
+        driver.switchTo().frame(iframe);
+        //find text in text box
+        WebElement text=driver.findElement(By.xpath("//p"));
+        //clear text box in iframe
+       text.clear();
+       //add new text to textbox
+       text.sendKeys("Hello World!");
+       //return to main page
+       driver.switchTo().defaultContent();
+    }
+}
